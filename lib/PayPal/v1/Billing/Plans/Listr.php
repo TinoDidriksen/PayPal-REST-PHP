70066bd77c5779159d75e407ce223ab4d5217133
Tino Didriksen <mail@tinodidriksen.com>
2022-03-08 17:11:12 +0100

    list is not a usable token

--- lib/PayPal/v1/Billing/Plans/Listr.php
+++ lib/PayPal/v1/Billing/Plans/Listr.php
@@ -2,7 +2,7 @@
 namespace PayPal\v1\Billing\Plans;
 use PayPalHttp\HttpRequest;
 
-class List extends HttpRequest {
+class Listr extends HttpRequest {
 	function __construct() {
 		parent::__construct('/v1/billing/plans/?', 'GET');
 		$this->headers['Content-Type'] = 'application/json';
--- lib/PayPal/v1/Catalogs/Products/Listr.php
+++ lib/PayPal/v1/Catalogs/Products/Listr.php
@@ -2,7 +2,7 @@
 namespace PayPal\v1\Catalogs\Products;
 use PayPalHttp\HttpRequest;
 
-class List extends HttpRequest {
+class Listr extends HttpRequest {
 	function __construct() {
 		parent::__construct('/v1/catalogs/products/?', 'GET');
 		$this->headers['Content-Type'] = 'application/json';
