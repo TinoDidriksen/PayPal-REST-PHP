c01b4953de7dd3f259cb08ca13bfe8cba69de25e
Tino Didriksen <mail@tinodidriksen.com>
2022-04-04 13:50:03 +0200

    Fix subscription transactions

--- lib/PayPal/v1/Billing/Subscriptions/Transactions.php
+++ lib/PayPal/v1/Billing/Subscriptions/Transactions.php
@@ -4,9 +4,17 @@ use PayPalHttp\HttpRequest;
 
 class Transactions extends HttpRequest {
 	function __construct($subId) {
-		parent::__construct('/v1/billing/subscriptions/{sub_id}/transactions?', 'POST');
+		parent::__construct('/v1/billing/subscriptions/{sub_id}/transactions?', 'GET');
 		$this->headers['Content-Type'] = 'application/json';
 
 		$this->path = str_replace('{sub_id}', urlencode($subId), $this->path);
+	}
+
+	public function start_time($p) {
+		$this->path .= "start_time=". urlencode($p) . "&";
+	}
+
+	public function end_time($p) {
+		$this->path .= "end_time=". urlencode($p) . "&";
 	}
 }
